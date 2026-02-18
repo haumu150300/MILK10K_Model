c2a71c3be82af6d643f1c41766005b0a5deb241d
Dexter <haunv1503@kw.ac.kr>
2026-02-18 20:59:42 +0900

    Update colab.py

--- colab.py
+++ colab.py
@@ -22,7 +22,7 @@ def train_one_epoch(
         inputs, labels = batch["dermoscopic"].to(device), batch["label"].to(device)
         optimizer.zero_grad()
         scaler = torch.amp.GradScaler(device)
-        with torch.cuda.amp.autocast(device, enabled=True):
+        with torch.cuda.amp.autocast("cuda", enabled=True):
             outputs = model(inputs)
             loss = criterion(outputs, labels)
         scaler.scale(loss).backward()
